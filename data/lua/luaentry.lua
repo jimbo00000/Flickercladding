eba93ab92345cf6560df08a8609651b327e24a68
Jim Susinno <james.susinno@gmail.com>
2016-09-27 21:38:46 -0400

    Add floorquad scene.

--- data/lua/luaentry.lua
+++ data/lua/luaentry.lua
@@ -14,6 +14,7 @@ local win_w,win_h = 800,800
 local lastSceneChangeTime = 0
 
 local scene_modules = {
+    "scene.floorquad",
     "scene.touchtris",
     "scene.colorquad",
     "scene.clockface",
