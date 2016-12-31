99c011126bc190c8f0b82045569d84e2ef957e1b
jimbo00000 <jim@jimbomania.com>
2016-12-31 13:44:58 -0500

    Fix up package path a bit for Android.

--- data/lua/luaentry.lua
+++ data/lua/luaentry.lua
@@ -1,6 +1,7 @@
 -- luaentry.lua
 local appDir = "/sdcard/Android/data/com.android.flickercladding"
-package.path = appDir..";../data/lua/?.lua;" .. package.path
+package.path = appDir.."/lua/?.lua;" .. package.path
+package.path = "../data/lua/?.lua;" .. package.path
 
 local ffi = require("ffi")
 local openGL -- @todo select GL or GLES header
